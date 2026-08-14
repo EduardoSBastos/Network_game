d784383422863ab5eb373132dc46880cb1112f03
EddSB <eduardosbastos@outlook.com>
2026-08-14 19:39:23 -0300

    make lobby show room names

--- Scenes/FusionTest/PhotonLobby/photon_lobby.gd
+++ Scenes/FusionTest/PhotonLobby/photon_lobby.gd
@@ -1,15 +1,20 @@
 class_name PhotonLobby extends Node2D
 
-@export var room_name_container: VBoxContainer
-@export var host_button: Button
+@export var room_name_list: ItemList
 @export var join_button: Button
+@export var refresh_button: Button
+@export var host_button: Button
 
 func _ready() -> void:
 	#Fusion.room_joined.connect(_on_room_joined)
+	join_button.pressed.connect(_on_start_button_pressed)
+	refresh_button.pressed.connect(update_rooms_list)
+	host_button.pressed.connect(create_room)
 	Fusion.register_broadcast_receiver(self)  # enables this node to receive broadcast RPCs
 	Fusion.connect_to_photon.call_deferred("user_%d" % randi())
-	Fusion.connected_to_photon.connect(update_rooms_list) # TODO: Remove this?
-	clear_players()
+	Fusion.connected_to_photon.connect(update_rooms_list)
+	room_name_list.clear()
+
 
 func create_room():
 	var options := FusionRoomOptions.new()
@@ -22,24 +27,13 @@ func create_room():
 
 
 func update_rooms_list():
+	room_name_list.clear()
 	var rooms: Array[FusionRoomListing] = Fusion.get_room_list()
 	for room in rooms:
 		print("===============================================")
 		print(room.name)
 		print(room.player_count)
-		add_room_to_lobby({"player": 1})
-
-
-func add_room_to_lobby(room_info):
-	var new_label: Label = Label.new()
-	new_label.text = room_info["name"]
-	room_name_container.add_child(new_label)
-
-
-func clear_players():
-	for child in room_name_container.get_children():
-		child.queue_free()
-
+		room_name_list.add_item(room.name)
 
 func _on_start_button_pressed() -> void:
 	Lobby.load_game.rpc("res://Scenes/high_level_example.tscn")
