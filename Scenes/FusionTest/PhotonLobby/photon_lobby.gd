dcbd3036ba72c258697fb2c116eedc097237d802
EddSB <eduardosbastos@outlook.com>
2026-08-23 20:32:34 -0300

    continue room screen

--- Scenes/FusionTest/PhotonLobby/photon_lobby.gd
+++ Scenes/FusionTest/PhotonLobby/photon_lobby.gd
@@ -5,26 +5,36 @@ class_name PhotonLobby extends Node2D
 @export var refresh_button: Button
 @export var host_button: Button
 @export var host_cancel_button: Button
+@export var create_room_button: Button
+@export var room_name_textedit: TextEdit
 @export var status_label:Label
 @export var lobby_panel:Panel
 @export var host_panel:Panel
+@export var room_panel:Panel
 
+var players:Dictionary
 
 func _ready() -> void:
 	join_button.pressed.connect(_on_join_button_pressed)
 	refresh_button.pressed.connect(update_rooms_list)
 	host_button.pressed.connect(_on_host_button_pressed)
 	host_cancel_button.pressed.connect(_on_host_cancel_button_pressed)
+	create_room_button.pressed.connect(_on_create_room_button_pressed)
+	
 	Fusion.register_broadcast_receiver(self)  # enables this node to receive broadcast RPCs
 	Fusion.connect_to_photon.call_deferred("user_%d" % randi())
 	
 	Fusion.connected_to_photon.connect(update_rooms_list)
 	Fusion.room_joined.connect(_on_room_joined)
+	Fusion.room_left.connect(_on_room_left)
+	Fusion.player_joined.connect(_on_player_joined)
+	Fusion.player_left.connect(_on_player_left)
 	Fusion.connection_failed.connect(_on_connection_failed)
 	room_name_list.clear()
 	
 	lobby_panel.show()
 	host_panel.hide()
+	room_panel.hide()
 
 
 func _on_host_button_pressed():
@@ -41,10 +51,15 @@ func _on_create_room_button_pressed():
 	var options := FusionRoomOptions.new()
 	options.max_players = 8
 	options.is_visible = true
+	var room_name:String = room_name_textedit.text
+	if room_name == "":
+		room_name = "test_%d" % Time.get_unix_time_from_system()
 	Fusion.create_room(
-		"test_%d" % Time.get_unix_time_from_system(),
+		room_name,
 		options
 	)
+	host_panel.hide()
+	room_panel.show()
 
 
 func update_rooms_list():
@@ -67,12 +82,36 @@ func _on_join_button_pressed() -> void:
 	Fusion.join_room(room_name)
 
 
+func _on_player_joined(player_id: int, user_id: String):
+	players[player_id] = {
+		"user_id": user_id
+	}
+
+	print("Player joined: ", player_id, " user: ", user_id)
+	update_player_list()
+
+
+func _on_player_left(player_id: int, is_inactive: bool):
+	if not is_inactive:
+		players.erase(player_id)
+	update_player_list()
+
+
+func update_player_list():
+	for player_id in players:
+		print(player_id, ": ", players[player_id].user_id)	
+
+
 func _on_room_joined():
 	print("Successfully joined room!")
 	print("Player ID: ", Fusion.get_local_player_id())
 	status_label.text = "Successfully joined room! Player ID: %s" % Fusion.get_local_player_id()
 	# TODO: Add Sccess Screen !!
 	# Start your game / change scene / spawn player here
+
+
+func _on_room_left():
+	pass
 
 
 func _on_connection_failed(error: String):
