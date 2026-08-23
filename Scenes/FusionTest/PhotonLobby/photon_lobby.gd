98124e16080c246d90c84ea8e060917849738af5
EddSB <eduardosbastos@outlook.com>
2026-08-23 15:20:05 -0300

    initiate host screen

--- Scenes/FusionTest/PhotonLobby/photon_lobby.gd
+++ Scenes/FusionTest/PhotonLobby/photon_lobby.gd
@@ -4,13 +4,17 @@ class_name PhotonLobby extends Node2D
 @export var join_button: Button
 @export var refresh_button: Button
 @export var host_button: Button
+@export var host_cancel_button: Button
 @export var status_label:Label
+@export var lobby_panel:Panel
+@export var host_panel:Panel
 
 
 func _ready() -> void:
 	join_button.pressed.connect(_on_join_button_pressed)
 	refresh_button.pressed.connect(update_rooms_list)
 	host_button.pressed.connect(_on_host_button_pressed)
+	host_cancel_button.pressed.connect(_on_host_cancel_button_pressed)
 	Fusion.register_broadcast_receiver(self)  # enables this node to receive broadcast RPCs
 	Fusion.connect_to_photon.call_deferred("user_%d" % randi())
 	
@@ -18,9 +22,22 @@ func _ready() -> void:
 	Fusion.room_joined.connect(_on_room_joined)
 	Fusion.connection_failed.connect(_on_connection_failed)
 	room_name_list.clear()
+	
+	lobby_panel.show()
+	host_panel.hide()
 
 
 func _on_host_button_pressed():
+	lobby_panel.hide()
+	host_panel.show()
+
+
+func _on_host_cancel_button_pressed():
+	host_panel.hide()
+	lobby_panel.show()
+
+	
+func _on_create_room_button_pressed():	
 	var options := FusionRoomOptions.new()
 	options.max_players = 8
 	options.is_visible = true
@@ -36,6 +53,7 @@ func update_rooms_list():
 	var rooms: Array[FusionRoomListing] = Fusion.get_room_list()
 	for room in rooms:
 		room_name_list.add_item(room.name)
+
 
 func _on_join_button_pressed() -> void:
 	var selected_items = room_name_list.get_selected_items()
@@ -55,6 +73,7 @@ func _on_room_joined():
 	status_label.text = "Successfully joined room! Player ID: %s" % Fusion.get_local_player_id()
 	# TODO: Add Sccess Screen !!
 	# Start your game / change scene / spawn player here
+
 
 func _on_connection_failed(error: String):
 	status_label.text = 'Connection Failed'
